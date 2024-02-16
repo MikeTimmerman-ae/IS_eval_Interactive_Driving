34e4bf8b018ab603acbba97581857be6fd7012c5
Mike Timmerman <timmermanmike@hotmail.com>
2024-02-15 17:32:19 -0800

    comment environment code

--- driving_sim/envs/t_intersection.py
+++ driving_sim/envs/t_intersection.py
@@ -14,46 +14,48 @@ from driving_sim.utils.trajectory import *
 
 from driving_sim.utils.info import *
 
-# Base class for the T-intersection env
-
 
 class TIntersection(TrafficEnv):
+    """ Base class for the T-intersection env """
     def __init__(self):
         super(TIntersection, self).__init__()
 
-        self.v_noise = None
-        self.vs_actions = None
-        self.t_actions = None
+        self.vs_actions = None          # Desired velocity actions for ego car: {0, 0.5, 3.0}
+        self.v_noise = None             # Noise on desired velocity: (v_noise * Uniform(-1, 1))
+        self.t_actions = None           # Desired side-ways deviation from trajectory: {0}
         # we use target value instead of target change so system is Markovian
-        self.rl_actions = None
-        self.num_updates = None
+        self.rl_actions = None          # Set of rl actions: {(0, 0), (0.5, 0), (3.0, 0)} - {(vs, t)}
+        self.num_updates = None         # Number of simulation steps per RL action step: 1
 
-        self.desire_speed = None
-        self.collision_cost = None
-        self.outroad_cost = None
-        self.survive_reward = None
-        self.goal_reward = None
+        self.desire_speed = None        # desired speed of the cars
+        self.collision_cost = None      # reward cost of collision
+        self.outroad_cost = None        # reward cost of vehicle going off road
+        self.survive_reward = None      # reward of survival
+        self.goal_reward = None         # reward of reaching goal
 
-        self.left_bound = None
-        self.right_bound = None
+        # Environment bounds and distances between cars
+        self.left_bound = None          # left bound for removing social car
+        self.right_bound = None         # right bound for removing social car
         self.gap_min = None
         self.gap_max = None
         self.max_veh_num = None
         self.label_num = self.max_veh_num
 
+        # Ego vehicle scenarios (collision, out road, goal reach)
         self._collision = False
         self._outroad = False # todo: seems that the ego car will never go out of road
         self._goal = False
         self._lower_lane_next_idx = 1
         self._upper_lane_next_idx = None
 
+        # Car properties
         self.car_length = 5.0
         self.car_width = 2.0
         self.car_max_accel = 10.0
         self.car_max_speed = 40.0
         self.car_max_rotation = np.pi / 18.
         self.car_expose_level = 4
-        self.driver_sigma = None
+        self.driver_sigma = None            # amount of noise on other drivers' actions
         self.s_des = 3.0
         self.s_min = 3.0
         self.min_overlap = 1.0
@@ -65,9 +67,8 @@ class TIntersection(TrafficEnv):
         self.agg_count = 0
         self.collided_veh_id = -999
 
-
-    # use the given config to configurate the environment
     def configure(self, config):
+        """ Configure environment attributes using given config, from driving configuration file """
         self.config = config
 
         self._road = config.env.road
@@ -117,30 +118,47 @@ class TIntersection(TrafficEnv):
         # P(conservative)
         self.yld_prob = config.env.con_prob
 
-
     def update(self, action):
+        """ Update the state of each car for 'num_updates' time steps using current action
+            1. Index and set selected RL action
+            2. Update environment state
+                2.1 get observations and actions for each vehicle
+                2.2 update car positions
+                2.3 run scenario checks: collision, deadlock, out road, goal reached
+                2.4 remove out-of-bound cars
+                2.5 add cars if enough space
 
-        # print(len(self._cars))
+            params: action {0, 1, 2} - index of selected RL action
+        """
 
-        rl_action = self.rl_actions[action]
+        ### 1. Index and set selected RL action ###
+        rl_action = self.rl_actions[action]         # index RL action from action space {(vs, t)}
+        # Set RL action for ego driver
         self._drivers[0].v_des = rl_action[0]
         self._drivers[0].t_des = rl_action[1]
 
+        ### 2. Update environment state ###
         self._goal = False
         self._collision = False
         self._outroad = False
         for _ in range(self.num_updates):
+            ## 2.1 Get observations and actions of each driver ##
             for driver in self._drivers:
                 driver.observe(self._cars, self._road)
             self._actions = [driver.get_action() for driver in self._drivers]
-            # create an action list that preserves the id of each driver
+
+            # Create an action list that preserves the id of each social driver
             self.action_with_idx = np.zeros(self.max_veh_num)
             for i, driver in enumerate(self._drivers):
                 if i == 0:  # skip the ego driver
                     continue
                 self.action_with_idx[int(driver._idx - 1)] = self._actions[i].a_x
+
+            ## 2.2 Update position of cars ##
             [action.update(car, self.dt) for (car, action) in zip(self._cars, self._actions)]
 
+            ## 2.3 Scenario checks ##
+            # Check if ego car collided
             ego_car = self._cars[0]
             for car in self._cars[1:]:
                 if ego_car.check_collision(car):
@@ -148,7 +166,7 @@ class TIntersection(TrafficEnv):
                     self.collided_veh_id = car._idx - 1
                     return
 
-            # deadlock check (deadlock == collision)
+            # Check deadlock (deadlock == collision)
             if 0.01 < ego_car.position[0] < 2.0:
                 for car in self._cars[1:]:
                     if 0.01 < car.position[0] < 6.0 and 1.5 < car.position[1] < 2.5:
@@ -156,17 +174,19 @@ class TIntersection(TrafficEnv):
                         self.collided_veh_id = car._idx - 1
                         return
 
+            # Check ego car going off-road
             if not self._road.is_in(ego_car):
                 self._outroad = True
                 return
 
+            # Check ego car reaching goal
             if (ego_car.position[0] > 8.) \
                 and (ego_car.position[1] > 5.) \
                 and (ego_car.position[1] < 7.):
                 self._goal = True
                 return
 
-            # remove cars that are out-of bound
+            ## 2.4 Remove social cars that are out-of bound ##
             for car, driver in zip(self._cars[1:], self._drivers[1:]):
                 if (car.position[1] < 4.) and (car.position[0] < self.left_bound):
                     self.remove_car(car, driver)
@@ -175,7 +195,7 @@ class TIntersection(TrafficEnv):
                     self.remove_car(car, driver)
                     removed_upper = True
 
-            # add cars when there is enough space
+            ## 2.5 Add cars when there is enough space ##
             # 1. find the right most car in the lower lane and the left most car in the upper lane and their idx
             # i.e. the cars that entered most recently in both lanes
             min_upper_x = np.inf
@@ -186,19 +206,16 @@ class TIntersection(TrafficEnv):
                 if (car.position[1] > 4.) and (car.position[0] < min_upper_x):
                     min_upper_x = car.position[0]
 
-            # add a car to both lanes if there is space
-
-            # lower lane
-            # decide the new car's yld = True or False
-            new_yld, gap_min, gap_max = self.init_trait()
+            # 2. add a car to both lanes if there is space
+            # -> lower lane
+            new_yld, gap_min, gap_max = self.init_trait()       # decide the new car's yld = True or False
             # condition for add a new car
             cond = max_lower_x < (self.right_bound - np.random.uniform(gap_min, gap_max) - self.car_length)
             # desired x location for the new car
             x = self.right_bound
-
-
+            # add car in lower lane if there is space
             if cond:
-                v_des = self.desire_speed + np.random.uniform(-1,1)*self.v_noise
+                v_des = self.desire_speed + np.random.uniform(-1, 1)*self.v_noise
                 p_des = 2.
                 direction = -1
 
@@ -207,15 +224,15 @@ class TIntersection(TrafficEnv):
                     car.setup_render(self.viewer)
                     driver.setup_render(self.viewer)
 
-            # upper lane
+            # -> upper lane
             new_yld, gap_min, gap_max = self.init_trait()
             # condition for adding a new car
             cond = min_upper_x > (self.left_bound + np.random.uniform(gap_min, gap_max) + self.car_length)
             # desired x location for the new car
             x = self.left_bound
-
+            # add car in upper lane if there is space
             if cond:
-                v_des = self.desire_speed + np.random.uniform(-1,1)*self.v_noise
+                v_des = self.desire_speed + np.random.uniform(-1, 1)*self.v_noise
                 p_des = 6.
                 direction = 1
 
@@ -224,14 +241,14 @@ class TIntersection(TrafficEnv):
                     car.setup_render(self.viewer)
                     driver.setup_render(self.viewer)
 
-
-    # determine whether the episode ended by checking all terminal conditions
     def is_terminal(self):
+        """ Determine whether the episode ended by checking all terminal conditions """
         return (self._collision or self._outroad or self._goal or self.global_time >= self.time_limit)
 
-
-    # get the info
     def get_info(self):
+        """ Get the info:
+            - info: timeout, collision, out road, reach goal, nothing
+        """
         info = {}
 
         if self.global_time >= self.time_limit:
@@ -247,13 +264,19 @@ class TIntersection(TrafficEnv):
         info['collision'] = self.collided_veh_id
         return info
 
-
-    # compute and return the observation
     def observe(self, normalize=True):
-        # if normalize: divide all positions by self.right_bound, divide all velocities by self.desire_speed
+        """ Compute and return the observation
+            obs: - robot node: position of ego car
+                 - temporal edges: velocity of ego car
+                 - spatial edges: relative position of social cars relative to ego car
+
+            params: normalize: divide all positions by self.right_bound, divide all velocities by self.desire_speed
+
+            return obs
+        """
         obs = {}
 
-        # update the yld belief every few timesteps
+        # update the yld belief every few timesteps (true class label of each car)
         if self.step_counter % 20 == 0:
             # print('update belief')
             self.yld_belief = np.zeros(self.max_veh_num)
@@ -263,6 +286,7 @@ class TIntersection(TrafficEnv):
                 else:
                     self.yld_belief[int(self._drivers[i]._idx-1)] = copy.deepcopy(self._drivers[i].yld)
 
+        # Compute observations
         obs['robot_node'] = self._cars[0].position/self.right_bound if normalize else self._cars[0].position
 
         # 2 classes
@@ -289,14 +313,12 @@ class TIntersection(TrafficEnv):
 
         return obs
 
-
-    # define the observation space
     @property
     def observation_space(self):
+        """ Define the observation space """
         d = {}
         # robot node: num_visible_humans, px, py
         d['robot_node'] = spaces.Box(low=-np.inf, high=np.inf, shape=(1, 2,), dtype=np.float32)
-
         # only consider all temporal edges (human_num+1) and spatial edges pointing to robot (human_num)
         d['temporal_edges'] = spaces.Box(low=-np.inf, high=np.inf, shape=(1, 2,), dtype=np.float32)
         # include ground truth information about whether other drivers will yield (0) or not (1)
@@ -305,36 +327,41 @@ class TIntersection(TrafficEnv):
                                         dtype=np.float32)
         return spaces.Dict(d)
 
-
-    # define the action space
     @property
     def action_space(self):
+        """ Define the action space """
         return spaces.Discrete(len(self.rl_actions))
 
-
-    # calculate the reward based on current state of ego car
     def get_reward(self):
+        """ Calculate the reward based on current state of ego car """
         reward = 0.
         ego_car = self._cars[0]
-        v_x, v_y = ego_car.velocity[0], ego_car.velocity[1]
 
         if self._collision:
+            # Cost of collision
             reward -= self.collision_cost
         elif self._outroad:
+            # Cost of out road
             reward -= self.outroad_cost
         elif self._goal:
+            # Reward of goal reached
             reward += self.goal_reward
         else:
-            # add reward for larger speeds & a small constant penalty to discourage the ego car from staying in place
-            reward = reward - self.survive_reward + 0.05 * np.linalg.norm([v_x, v_y]) / 3
+            # Reward while ego is still driving:
+            # 1. add reward for larger speeds
+            # 2. subtract small constant penalty to discourage the ego car from staying in place
+            reward = reward - self.survive_reward + 0.05 * np.linalg.norm(ego_car.velocity) / 3
             # reward = reward + 0.01 * np.linalg.norm([v_x, v_y]) / 3
 
         # print('reward:', reward)
         return reward
 
-
-    # remove the given car and its corresponding controller (driver)
     def remove_car(self, car, driver):
+        """ Remove the given car and its corresponding controller (driver)
+
+            params: car: car instance to be removed from list [Car]
+                    driver: driver instance to be removed from list [Driver]
+        """
         self._cars.remove(car)
         self._drivers.remove(driver)
         if hasattr(self, 'viewer') and self.viewer:
@@ -344,42 +371,56 @@ class TIntersection(TrafficEnv):
         self.car_present[int(car._idx - 1)] = False
         self.car_lane_info[int(car._idx - 1)] = 0
 
-
-    # given the pose of a car, initialize a Car & a Driver instance and append them to self._cars & self._drivers
     def add_car(self, x, y, vx, vy, v_des, p_des, direction, theta, yld):
-        # P(conservative)
+        """ Initialize a Car & a Driver instance and append them to self._cars & self._drivers
+
+            params: (x, y, vx, vy) - pose   [float]
+                    v_des, p_des - desired speed [m/s]  [float]
+                    direction - driving direction (-1: to left, +1: to right)   [int]
+                    theta - orientation of car w.r.t. positive x-axis   [float]
+                    yld - whether the car yields or not (true label)    [bool]
+        """
+        # P(conservative) - use counts to estimate probability
         if yld:
             self.con_count = self.con_count + 1
         else:
             self.agg_count = self.agg_count + 1
+
         if y < 4.:
+            # Add car to lower lane
             idx = self._lower_lane_next_idx
             self._lower_lane_next_idx += 1
             if self._lower_lane_next_idx > int(self.max_veh_num/2.):
                 self._lower_lane_next_idx = 1
         elif y > 4.:
+            # Add car to upper lane
             idx = self._upper_lane_next_idx
             self._upper_lane_next_idx += 1
             if self._upper_lane_next_idx > self.max_veh_num:
                 self._upper_lane_next_idx = int(self.max_veh_num/2.)+1
+
+        # Create car instance
         car = Car(idx=idx, length=self.car_length, width=self.car_width, color=random.choice(RED_COLORS),
-                          max_accel=self.car_max_accel, max_speed=self.car_max_speed,
-                          max_rotation=0.,
-                          expose_level=self.car_expose_level)
+                  max_accel=self.car_max_accel, max_speed=self.car_max_speed, max_rotation=0.,
+                  expose_level=self.car_expose_level)
+        # Create driver instance
         driver = YNYDriver(idx=idx, car=car, dt=self.dt,
                     x_driver=IDMDriver(idx=idx, car=car, sigma=self.driver_sigma, s_des=self.s_des, s_min=self.s_min,
                                        axis=0, min_overlap=self.min_overlap, dt=self.dt),
-                    y_driver=PDDriver(idx=idx, car=car, sigma=0., axis=1, dt=self.dt)) 
+                    y_driver=PDDriver(idx=idx, car=car, sigma=0., axis=1, dt=self.dt))
+
+        # Configure car
         car.set_position(np.array([x, y]))
         car.set_init_position(np.array([x, y]))
         car.set_velocity(np.array([vx, vy]))
         car.set_rotation(theta)
+        # Configure driver
         driver.x_driver.set_v_des(v_des)
         driver.x_driver.set_direction(direction)
         driver.y_driver.set_p_des(p_des)
-
         driver.set_yld(yld)
 
+        # Append car and driver to environment
         self._cars.append(car)
         self._drivers.append(driver)
 
@@ -388,8 +429,13 @@ class TIntersection(TrafficEnv):
 
         return car, driver
 
-    # randomly initialize yld for a new car, returns the yld and desired front gap depending on yld
     def init_trait(self, reset=False):
+        """ Randomly initialize yld for a new car
+
+            params: reset - if True, use new yld_prob
+            returns: - yld: car's trait class [conservative/aggressive] [bool]
+                     - gap_min, gap_max: desired min/max front gap, depending on yld [float]
+        """
         if reset:
             # todo: hardcoded for now
             if self.yld_prob == 0.67:
@@ -399,11 +445,15 @@ class TIntersection(TrafficEnv):
         else:
             yld_prob = self.yld_prob
 
+        # Determine the car's class (cons/aggr)
         if np.random.uniform() < yld_prob:
+            # Conservative trait
             new_yld = True
         else:
+            # Aggressive trait
             new_yld = False
-        # determine the range of front gap from the car's class (cons/agg)
+
+        # Determine the range of front gap from the car's class (cons/agg)
         if new_yld:
             gap_min = self.con_gap_min
             gap_max = self.con_gap_max
@@ -413,8 +463,8 @@ class TIntersection(TrafficEnv):
 
         return new_yld, gap_min, gap_max
 
-    # true reset function (will be called in reset in traffic_env.py)
     def _reset(self):
+        """ True reset function (called in reset in traffic_env.py) """
         self.collided_veh_id = -999
         self._collision = False
         self._outroad = False
@@ -468,7 +518,6 @@ class TIntersection(TrafficEnv):
             self.add_car(x, p_des, v_des, 0., v_des, p_des, direction, 0., new_yld)
             new_yld, gap_min, gap_max = self.init_trait(reset=True)
             x -= (np.random.uniform(gap_min, gap_max) + self.car_length)
-
 
     def setup_viewer(self):
         from driving_sim import rendering
--- driving_sim/utils/trajectory.py
+++ driving_sim/utils/trajectory.py
@@ -1,45 +1,72 @@
 import numpy as np
 
+
 # defines the trajectory of the ego car
-# todo: what does s, t, theta, curv mean?
 class EgoTrajectory:
+    """ Translates between trajectory coordinates (s, t) and cartesian coordinates (x, y)
+            Trajectory is defined to be a right-turning motion with the turn starting at (x, y) = (0, 0)
+            turning radius is 6m, from (x, y) = (0, 0) to (x, y) = (6, 6)
+            before the turn: car is on a straight trajectory parallel to y-axis
+            after the turn: car is on a straight trajectory parallel to x-axis
+        - s: distance travelled along trajectory
+        - t: transverse distance from trajectory (side-ways deviation)
+        - theta: orientation angle from position x-axis
+        - curve: curvature of current part of trajectory (1/radius)
+    """
+
     def xy_to_traj(self, pos):
+        """ Translates cartesian coordinates (x, y) to trajectory coordinates (s, t)
+
+            params: pos (x, y) - cartesian coordinates   [np.array]
+            return: (s, t, theta, curv) - trajectory coordinates, and rotation and curvature
+        """
         x, y = pos[0], pos[1]
         r = 6 # 4.5
         if y < 0.:
+            # Car is before the turning motion
             s = y
             t = -x
             theta = np.pi/2
             curv = 0.
         elif x > r:
+            # Car is beyond the turning motion
             s = r*np.pi/2. + x - r
             t = y - r
             theta = 0.
             curv = 0.
         else:
-            theta = np.arctan2(r-x ,y)
+            # Car is in turning motion
+            theta = np.arctan2(r-x, y)
             curv = 1./r
             s = r*(np.pi/2.-theta)
-            t = np.sqrt((r-x)**2+(y)**2) - r
+            t = np.sqrt((r-x)**2 + y**2) - r
 
         return s, t, theta, curv
 
     def traj_to_xy(self, pos):
+        """ Translates trajectory coordinates (s, t) to cartesian coordinates (x, y)
+
+            params: pos (s, t) - coordinates along trajectory   [np.array]
+            return: (x, y, theta, curv) - cartesian coordinates, and rotation and curvature
+        """
         s, t = pos[0], pos[1]
         r = 6 # 4.5
         if s < 0.:
-            x = -t
-            y = s
-            theta = np.pi/2
-            curv = 0.
+            # Car is before the turning motion
+            x = -t                      # side-ways deviation in x-direction with t [m]
+            y = s                       # position along y-direction
+            theta = np.pi/2             # orientation in positive y-direction
+            curv = 0.                   # driving on straight road
         elif s > r*np.pi/2.:
-            x = r + s - r*np.pi/2.
-            y = r + t
-            theta = 0.
-            curv = 0.
+            # Car is beyond the turning motion
+            x = r + s - r*np.pi/2.      # position along x-direction
+            y = r + t                   # side-ways deviation in y-direction with t [m]
+            theta = 0.                  # orientation in positive x-direction
+            curv = 0.                   # driving on straight road
         else:
-            theta = np.pi/2 - s/r
-            curv = 1./r
+            # Car is in turning motion
+            theta = np.pi/2 - s/r       # orientation along curve
+            curv = 1./r                 # curvature of turning motion
             x = r - (r+t)*np.sin(theta)
             y = (r+t)*np.cos(theta)
 
