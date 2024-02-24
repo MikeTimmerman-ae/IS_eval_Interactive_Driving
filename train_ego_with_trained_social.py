857327326e7c8e5cdbc27a0a6192091e0368a734
Mike Timmerman <timmermanmike@hotmail.com>
2024-02-24 15:52:25 -0800

    test load social model

--- train_ego_with_trained_social.py
+++ train_ego_with_trained_social.py
@@ -156,6 +156,7 @@ def main():
 
     actor_critic_social = SocialPolicy(envs.observation_space.spaces, envs.action_space,
                                        base_kwargs=social_config, base=social_config.env_config.robot.policy, meta=True)
+    os.mkdir("data/test/")
     torch.save(actor_critic_social.state_dict(), "data/test/social.pt")
     actor_critic_social.load_state_dict(torch.load("data/test/social.pt"))
     if not test_args.use_idm:
