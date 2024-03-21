1d9e6ac77722eddb97ce3cf2d8cd1a3c810ae4fe
Mike Timmerman <timmermanmike@hotmail.com>
2024-03-21 16:07:35 -0700

    bug fix

--- configs/ego_config/driving_config.py
+++ configs/ego_config/driving_config.py
@@ -26,7 +26,7 @@ class DrivingConfig(object):
     # 0.88, reset = 0.6 -> P(con) = 0.61, P(agg) = 0.39
     env.con_prob = 0.75
     # Beta distribution parameters
-    env.mean = 0.5
+    env.mean = 1.5
     env.std = 0.5
 
     env.test_size = 500
