151a77c9d930e6697ce4d1af546bcf0848bdd9e3
Mike Timmerman <timmermanmike@hotmail.com>
2024-03-21 16:33:01 -0700

    bug fix

--- rl/envs.py
+++ rl/envs.py
@@ -44,8 +44,8 @@ def make_env(env_id, seed, rank, log_dir, allow_early_resets, config=None, envNu
             env.unwrapped, gymnasium.envs.atari.atari_env.AtariEnv)
         if is_atari:
             env = make_atari(env_id)
-        print("in: ", config.env.std)
-        env.configure(config.env_config, envNum, config.env.mean, config.env.std)
+        print("in: ", config.env_config.env.std)
+        env.configure(config.env_config, envNum, config.env_config.env.mean, config.env_config.env.std)
 
         envSeed = seed + rank if seed is not None else None
 
