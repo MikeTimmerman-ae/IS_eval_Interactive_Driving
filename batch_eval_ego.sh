d17bb4757230035bce5fa595d9d3867d9ca70ddd
Mike Timmerman <timmermanmike@hotmail.com>
2024-04-03 09:39:42 -0700

    increase evaluation samples

--- batch_eval_ego.sh
+++ batch_eval_ego.sh
@@ -8,7 +8,7 @@
 # 3. Specify evaluation distribution (mu, sigma)
 ###############################################################################
 
-num_eval=10
+num_eval=5000
 experiment=experiment_2
 
 mean_naturalistic=2.0
