0d4b76843930c3edf12c9d3b4113ea2090260482
Mike Timmerman <timmermanmike@hotmail.com>
2024-04-03 14:59:38 -0700

    run eval batch

--- batch_eval_ego.sh
+++ batch_eval_ego.sh
@@ -8,7 +8,7 @@
 # 3. Specify evaluation distribution (mu, sigma)
 ###############################################################################
 
-num_eval=50
+num_eval=5
 experiment=experiment_2
 
 mean_naturalistic=2.0
