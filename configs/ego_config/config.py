1284fe441020fec5d4cd74c452ae4eb63fdbcb24
Mike Timmerman <timmermanmike@hotmail.com>
2024-05-01 22:26:56 -0700

    change number of PPO mini batches

--- configs/ego_config/config.py
+++ configs/ego_config/config.py
@@ -17,7 +17,7 @@ class Config(object):
 
     # ppo config
     ppo = BaseConfig()
-    ppo.num_mini_batch = 2  # number of batches for ppo
+    ppo.num_mini_batch = 1  # number of batches for ppo
     ppo.num_steps = 30  # number of forward steps
     ppo.recurrent_policy = True  # use a recurrent policy
     ppo.epoch = 5  # number of ppo epochs
